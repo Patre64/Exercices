742c87abbcb5e3c56c45c253f4758242be34ac16
Patre64 <38065364+Patre64@users.noreply.github.com>
2018-07-06 14:46:04 +0200

    Add files via upload

--- JQuery/Exo5_jq/js/exo5_jq.js
+++ JQuery/Exo5_jq/js/exo5_jq.js
@@ -1,6 +1,37 @@
-$(function(){
+$(function () {
 
-
-
+    /* $("#p1").hover(function(){
+        $("#p5").hide();
+    },
+    function(){
+        $("#p5").show();
+    });
     
+    }); */
+
+
+    /* $("#p1").on({
+            mouseenter: function () {
+                $("#p5").hide();
+            },
+            mouseleave: function () {
+                $("#p5").show();
+            },
+            click: function () {
+                $("#p3").hide();
+            }
+    
+        });
+    }); */
+
+
+    $("#p1").click(function () {
+        $("#p2").hide(1005, function () {
+            $("#p3").hide(1000, function () {
+                $("#p4").hide(1000, function () {
+                    $("#p5").hide(1000); 
+                });   
+            });   
+        });
+    });
 });
