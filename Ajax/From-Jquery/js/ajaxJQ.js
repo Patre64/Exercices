ea12a5c42a3694e522f3a53532b80d418430e9a1
Patre64 <38065364+Patre64@users.noreply.github.com>
2018-07-24 10:17:09 +0200

    Add files via upload

---
+++ Ajax/From-Jquery/js/ajaxJQ.js
@@ -0,0 +1,76 @@
+/* class Fromages {
+    constructor(nom, type, pays, classement) {
+        this.nom = nom;
+        this.type = type;
+        this.pays = pays;
+        this.classement = classement;
+    }
+
+    afficher() {
+        let maDiv = document.createElement("div");
+        let monNom = document.createElement("h1");
+        let monClass = document.createElement("p");
+        let monType = document.createElement("p");
+        let monPays = document.createElement("p");
+
+        monNom.innerText = this.nom;
+        monClass.innerText = this.classement;
+        monType.innerText = this.type;
+        monPays.innerText = this.pays;
+
+        maDiv.classList.add("container");
+        monNom.classList.add("nom");
+        monClass.classList.add("etoile");
+        monType.classList.add("type");
+        monPays.classList.add("pays"),
+
+        maDiv.appendChild(monNom);
+        maDiv.appendChild(monClass);
+        maDiv.appendChild(monType);
+        maDiv.appendChild(monPays);
+
+        return maDiv;
+
+    }
+
+} */
+
+
+$.getJSON( "ajax/ajaxJQ.json", function( data ) {
+    var items = [];
+    $.each( data, function( key, val ) {
+      items.push( "<li id='" + key + "'>" + val + "</li>" );
+    });
+   
+    $( "<ul/>", {
+      "class": "my-new-list",
+      html: items.join( "" )
+    }).appendTo( "body" );
+  });
+
+
+
+
+
+
+/* var xhr = new XMLHttpRequest();
+xhr.open("GET", "json/ajaxJQ.json");
+xhr.responseType = "json";
+xhr.onreadystatechange = function () {
+
+    if (this.readyState == 4 && this.status == 200) {
+        //console.log("ok");
+        let reponseJson = this.response;
+        //console.log(reponseJson);
+        let article = document.createElement("article");
+        for (let element of reponseJson) {
+            //console.log(element);
+            let fromage = new Fromages(element.nom, element.classement, element.type, element.pays);
+            article.appendChild(fromage.afficher());
+        }
+        document.body.appendChild(article);
+    }
+
+};
+
+xhr.send() */
